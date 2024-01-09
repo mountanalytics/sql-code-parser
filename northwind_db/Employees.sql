35f46374694ffeee4c2ace16b8fc6130c2153cfd
mountanalytics <150941230+mountanalytics@users.noreply.github.com>
2024-01-09 13:40:54 +0100

    Create Employees.sql

---
+++ northwind_db/Employees.sql
@@ -0,0 +1,2 @@
+SELECT EmployeeID, LastName, FirstName, Title, TitleOfCourtesy, BirthDate, HireDate, Address, City, Region, PostalCode, Country, HomePhone, Extension, ReportsTo
+FROM MA_NorthWindDB.dbo.Employees;
