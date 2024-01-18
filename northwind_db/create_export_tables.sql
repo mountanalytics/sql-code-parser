49abba12ec4029d50dace0eb7bc0a65a2028abb3
mountanalytics <150941230+mountanalytics@users.noreply.github.com>
2024-01-18 17:53:14 +0100

    Update create_export_tables.sql
    removed subqueries for order details

--- northwind_db/create_export_tables.sql
+++ northwind_db/create_export_tables.sql
@@ -53,19 +53,75 @@ select
 				OrderID;
 
 
-INSERT INTO MA_NorthWindDB.dbo.Order_Details_Product_AGGR (orderid, NrOfProducts, avg_order_unitprice, max_order_discount, min_order_discount, total_quantity)
+INSERT INTO MA_NorthWindDB.dbo.Order_Details_Product_AGGR (productid, product_quantity_year)
 select 
 				productid, 
-				count(*) as NrOfProducts, 
-				avg(unitprice) as avg_order_unitprice,
-				max(Discount) as max_order_discount,  
-				min(Discount) as min_order_discount, 
-				sum(quantity) as total_quantity
+				sum(quantity) as product_quantity_year
 			from 
 				dbo.[Order Details] ode 
 			group by 
-				OrderID;
+				productid;
 
+INSERT INTO MA_NorthWindDB.dbo.Order_Details_1996_EXPORT(OrderID, ProductID, UnitPrice, Quantity, Discount, NrOfProducts, avg_order_unitprice, max_order_discount, min_order_discount, total_quantity, product_quantity_year, 	perc_of_product_quantity_year)
+SELECT 
+	a.*, 
+	c.NrOfProducts, 
+	c.avg_order_unitprice,
+	c.max_order_discount, 
+	c.min_order_discount, 
+	c.total_quantity,
+	d.product_quantity_year, 
+	round((cast(a.quantity as numeric (10,2))/cast(d.product_quantity_year as numeric (10,2)))*100,1) as perc_of_product_quantity_year
+FROM 
+	MA_NorthWindDB.dbo.[Order Details] a INNER JOIN MA_NorthWindDB.dbo.Orders b
+	on a.OrderID = b.OrderID 
+	INNER JOIN MA_NorthWindDB.dbo.Order_Details_Order_AGGR c
+	on a.OrderID = c.OrderID	
+	INNER JOIN MA_NorthWindDB.dbo.Order_Details_Product_AGGR d
+	on a.productID = d.productID
+where 
+	YEAR(b.OrderDate) = 1996;
+
+
+INSERT INTO MA_NorthWindDB.dbo.Order_Details_1997_EXPORT(OrderID, ProductID, UnitPrice, Quantity, Discount, NrOfProducts, avg_order_unitprice, max_order_discount, min_order_discount, total_quantity, product_quantity_year, 	perc_of_product_quantity_year)
+SELECT 
+	a.*, 
+	c.NrOfProducts, 
+	c.avg_order_unitprice,
+	c.max_order_discount, 
+	c.min_order_discount, 
+	c.total_quantity,
+	d.product_quantity_year, 
+	round((cast(a.quantity as numeric (10,2))/cast(d.product_quantity_year as numeric (10,2)))*100,1) as perc_of_product_quantity_year
+FROM 
+	MA_NorthWindDB.dbo.[Order Details] a INNER JOIN MA_NorthWindDB.dbo.Orders b
+	on a.OrderID = b.OrderID 
+	INNER JOIN MA_NorthWindDB.dbo.Order_Details_Order_AGGR c
+	on a.OrderID = c.OrderID	
+	INNER JOIN MA_NorthWindDB.dbo.Order_Details_Product_AGGR d
+	on a.productID = d.productID
+where 
+	YEAR(b.OrderDate) = 1997;
+
+INSERT INTO MA_NorthWindDB.dbo.Order_Details_1998_EXPORT(OrderID, ProductID, UnitPrice, Quantity, Discount, NrOfProducts, avg_order_unitprice, max_order_discount, min_order_discount, total_quantity, product_quantity_year, 	perc_of_product_quantity_year)
+SELECT 
+	a.*, 
+	c.NrOfProducts, 
+	c.avg_order_unitprice,
+	c.max_order_discount, 
+	c.min_order_discount, 
+	c.total_quantity,
+	d.product_quantity_year, 
+	round((cast(a.quantity as numeric (10,2))/cast(d.product_quantity_year as numeric (10,2)))*100,1) as perc_of_product_quantity_year
+FROM 
+	MA_NorthWindDB.dbo.[Order Details] a INNER JOIN MA_NorthWindDB.dbo.Orders b
+	on a.OrderID = b.OrderID 
+	INNER JOIN MA_NorthWindDB.dbo.Order_Details_Order_AGGR c
+	on a.OrderID = c.OrderID	
+	INNER JOIN MA_NorthWindDB.dbo.Order_Details_Product_AGGR d
+	on a.productID = d.productID
+where 
+	YEAR(b.OrderDate) = 1998;
 
 
   
